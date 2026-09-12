59865391e878186a87b62cc603c016c85f23909e
luiscantillo <lcantillo@jamar.com>
2026-09-12 15:11:16 -0500

    Declaración de listas y las variables globales para informe de caja

--- 1ERPARCIAL/Program.cs
+++ 1ERPARCIAL/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Declaro las listas donde voy a almacenar los nombres de producto, precios, stocks y 
+    // la lista de cual producto tuvo mas ventas
+    static List<string> nombresArticulo = new List<string>();
+    static List<decimal> preciosArticulo = new List<decimal>();
+    static List<int> stocksArticulo = new List<int>();
+    static List<int> ventasPorProducto = new List<int>(); // Para saber cuál se vende más
+
+    // Variables para el reporte de caja
+    static int totalCantVentas = 0;
+    static decimal totalIgresosCaja = 0m; 
+    
+    
+    
+    }
